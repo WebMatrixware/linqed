2aaa75c57e0be8f085e34a64e5906a2ab1dfeba1
Ben Lanning <blanning@all-mode.com>
2020-07-14 15:23:21 -0400

    Add .gitignore

--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@
 
 let linqed = function(array) {
   
+  // Return true if collection contains element, false otherwise
+  let __contains = function __contains(filter) {
+    
+    let i = 0;
+    
+    if (typeof(filter) === 'undefined') {
+      return this;
+    }
+    
+    if (typeof(filter) === 'function') {
+      for(i = 0; i < this.length; ++i) {
+        if (filter(this[i])) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      if (this.indexOf(filter) !== -1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+  
   // Return an empty collection, usefull sometimes
   // for resetting a collection
   let __empty = function __empty() {
@@ -14,6 +39,8 @@ let linqed = function(array) {
   // Return a merged array joined on the filter function
   // provided
   let __join = function __join(outer, filter) {
+    
+    let mergedArray = linqed([]);
     
     if (!Array.isArray(outer)
        || typeof(filter) !== 'function') {
@@ -34,6 +61,10 @@ let linqed = function(array) {
     return this.filter(filter);
   };
   
+  Object.defineProperty(array, 'contains', {
+    enumerable: false,
+    value: __contains
+  });
   Object.defineProperty(array, 'empty', {
     enumerable: false,
     value: __empty
---
+++ test/contains.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Linqed = require('../src/index.js');
+const test = require('ava');
+
+test.beforeEach((t) => {
+  
+  t.context = Linqed([1, 2, 3, 4]);
+});
+
+test('.contains() exists', (t) => {
+  
+  t.truthy(t.context.hasOwnProperty('contains'));
+});
+
+test('.contains() is not enumerable', (t) => {
+  
+  t.falsy(t.context.propertyIsEnumerable('contains'));
+});
+
+test('.contains() with no filter returns unaltered array', (t) => {
+  
+  t.is(t.context.contains().length, 4);
+});
+
+test('.contains() with string or number returns true if it is a member of the array', (t) => {
+  
+  t.truthy(t.context.contains(1));
+});
+
+test('.contains() with a string or number returns false if it is not a member of the array', (t) => {
+  
+  t.falsy(t.context.contains(5));
+});
+
+test('.contain() can handle objects with properties', (t) => {
+  
+  let testArray = Linqed([
+    { name: 'Ben', id: 1, car: 'Versa Note' },
+    { name: 'Dale', id: 2, car: 'Versa' },
+    { name: 'Bill', id: 3, car: 'Versa Note' },
+    { name: 'Rob', id: 4 }
+  ]);
+  
+  t.truthy(testArray.contains((item) => { if (item.id === 1) return item; }));
+  t.falsy(testArray.contains((item) => { if (item.id === 5) return item; }));
+});
